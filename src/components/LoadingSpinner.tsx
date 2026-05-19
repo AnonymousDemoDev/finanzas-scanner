e75148ed6c04301f431fd81904d72bda60d6cc1c
AnonymousDemoDev <usernokiademo@gmail.com>
2026-05-19 17:44:18 -0300

    vaciar repositorio

--- src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-emerald-500"></div>
-    </div>
-  );
-}
